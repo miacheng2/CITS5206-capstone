855963b61998d081d5988e43503f3806fadc3164
ivy <ivyhzyb@gmail.com>
2024-08-24 22:49:49 +0800

    Deploy whole database model: added user, team; linked tables together

--- myproject/events/models.py
+++ myproject/events/models.py
@@ -1,4 +1,68 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, user_type, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            user_type=user_type,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, user_type='admin', password=None):
+        user = self.create_user(
+            email=email,
+            username=username,
+            user_type=user_type,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    ADMIN = 'admin'
+    TEAM_LEADER = 'team_leader'
+    USER_TYPE_CHOICES = [
+        (ADMIN, 'Admin'),
+        (TEAM_LEADER, 'Team Leader'),
+    ]
+    
+    email = models.EmailField(primary_key=True, unique=True)
+    username = models.CharField(max_length=50)
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
+    
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'user_type']
+    
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
 
 class TeamMember(models.Model):
     SENIOR_SAILING = 'Senior sailing membership'
@@ -22,6 +86,7 @@ class TeamMember(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     membership_category = models.CharField(max_length=50, choices=MEMBERSHIP_CATEGORY_CHOICES)
+    teams = models.ManyToManyField(Team, related_name='members', blank=True)
 
 class Event(models.Model):
     EVENT_TYPE_CHOICES = (
@@ -31,12 +96,16 @@ class Event(models.Model):
     name = models.CharField(max_length=100)
     event_type = models.CharField(max_length=10, choices=EVENT_TYPE_CHOICES)
     date = models.DateField()
+    # allow null for now, will change it later
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
 class VolunteerPoints(models.Model):
     member = models.ForeignKey(TeamMember, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     points = models.IntegerField()
     hours = models.IntegerField(null=True, blank=True)  # Only for off-water events
+    # allow null for now, will change it later
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         if self.event.event_type == 'off_water' and self.hours:
