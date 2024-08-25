ad13e409b1acd6decec055f81d82152bad49fab8
Ninu <ninupj93@gmail.com>
2024-08-25 19:26:44 +0800

    Modified home section

--- Front_end/src/components/Home.js
+++ Front_end/src/components/Home.js
@@ -1,19 +1,60 @@
 import React from 'react';
-
 import './home.css';  
 
-const Home = () => (  
-    <div>
-       
-        <header className="home-section">
-            <div className="overlay">
-                <div className="text-container">
-                    <h2>Nedlands Yacht Club Volunteer Management</h2>
-                    <p>Empowering the community, one volunteer at a time.</p>
-                </div>
-            </div>
-        </header>
-    </div>
+const Home = () => (
+  <div className="homepage">
+    <header className="home-section">
+      <div className="overlay">
+        <div className="text-container">
+          <h2>Nedlands Yacht Club Volunteer Management</h2>
+          <p>Empowering the community, one volunteer at a time.</p>
+        </div>
+      </div>
+    </header>
+
+    {/* Main Card Container */}
+    <section className="homepage-card-container">
+      <div className="homepage-card yellow-card">
+        <div className="homepage-card-content">
+          <h2>Volunteer Points</h2>
+          <p>Track volunteer hours, assign points, and reward contributions.</p>
+        </div>
+        <div className="homepage-card-hover">
+          <button onClick={() => window.location.href='/volunteer-points'}>Learn More</button>
+        </div>
+      </div>
+
+      <div className="homepage-card green-card">
+        <div className="homepage-card-content">
+          <h2>Plan & Manage Events</h2>
+          <p>Organize events to engage and mobilize volunteers.</p>
+        </div>
+        <div className="homepage-card-hover">
+          <button onClick={() => window.location.href='/events'}>Learn More</button>
+        </div>
+      </div>
+      
+      <div className="homepage-card orange-card">
+        <div className="homepage-card-content">
+          <h2>Team Overview</h2>
+          <p>Organize teams and assign roles for efficient management.</p>
+        </div>
+        <div className="homepage-card-hover">
+          <button onClick={() => window.location.href='/WorkTeamManagement'}>Learn More</button>
+        </div>
+      </div>
+      
+      <div className="homepage-card blue-card">
+        <div className="homepage-card-content">
+          <h2>Performance Reports</h2>
+          <p>Generate and analyze volunteer performance and event impact.</p>
+        </div>
+        <div className="homepage-card-hover">
+          <button onClick={() => window.location.href='/reports'}>Learn More</button>
+        </div>
+      </div>
+    </section>
+  </div>
 );
 
 export default Home;
