19928e9e2297df46cc1029fd325d3e82ff753267
Hei <nanx.rao@gmail.com>
2024-09-02 16:50:08 +0800

    conflit fixed

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 from events.views import (TeamMemberViewSet, EventViewSet, VolunteerPointsViewSet, ChangePasswordView, UpdateProfileView,
-CreateAdminUserView,RegisterView,GetProfileView,UpdateProfileView, UserViewSet, TeamViewSet, TeamMemberViewSet, EventViewSet, VolunteerPointsViewSet,AllMembersPointsAPIView,save_volunteer_points) 
+CreateAdminUserView,RegisterView,GetProfileView,UpdateProfileView, UserViewSet, TeamViewSet, TeamMemberViewSet, EventViewSet, VolunteerPointsViewSet,AllMembersPointsAPIView,LoginView,save_volunteer_points,PromoteLeaderView)
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -13,16 +13,17 @@ router.register(r'events', EventViewSet)
 router.register(r'volunteer-points', VolunteerPointsViewSet)
 
 urlpatterns = [
+    path('api/login/', LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/token-auth/', obtain_auth_token),
     path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('api/promote-leader/', PromoteLeaderView.as_view(), name='promote_leader'),
     path('api/update-profile/', UpdateProfileView.as_view(), name='update-profile'),
     path('api/create-admin/', CreateAdminUserView.as_view(), name='create-admin'),
+   
     path('api/register/', RegisterView.as_view(), name='register'),
     path('api/get-profile/', GetProfileView.as_view(), name='get-profile'),
     path('api/members-points-all/', AllMembersPointsAPIView.as_view(), name='all-members-points'),
     path('api/save-volunteer-points/', save_volunteer_points, name='save-volunteer-points'),
     path('upload_csv/', include('upload_csv.urls'), name='upload_csv'),
 ]
-
